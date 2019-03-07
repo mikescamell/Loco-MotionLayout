c7a3d3750a07b080ee62e0628e4ddf94dcd627d9
Mike Scamell <mikescamell@gmail.com>
2019-03-07 12:15:33 -0600

    Add part 6 showing KeyAttributes

--- app/src/main/java/com/mikescamell/locomotionlayout/MainActivity.kt
+++ app/src/main/java/com/mikescamell/locomotionlayout/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout1_part5)
+        setContentView(R.layout.layout1_part6)
     }
 }
