6f199192bf7fd1b3b35e2384830d8d9c7d4038a1
Mike Scamell <mikescamell@gmail.com>
2019-05-01 13:45:48 +0100

    Add base monzo fan out animation

---
+++ app/src/main/java/com/mikescamell/locomotionlayout/MonzoActivity.kt
@@ -0,0 +1,14 @@
+package com.mikescamell.locomotionlayout
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MonzoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.monzo_cards)
+    }
+}
