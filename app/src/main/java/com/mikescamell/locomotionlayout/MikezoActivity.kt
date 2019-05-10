679ba836c13a640243f31ea9012385e109643452
Mike Scamell <mikescamell@gmail.com>
2019-05-10 15:53:09 +0100

    Add fan out and selecting each card with auto animation at start

--- app/src/main/java/com/mikescamell/locomotionlayout/MikezoActivity.kt
+++ app/src/main/java/com/mikescamell/locomotionlayout/MikezoActivity.kt
@@ -2,7 +2,10 @@ package com.mikescamell.locomotionlayout
 
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionScene
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.mikezo_cards.*
@@ -15,6 +18,12 @@ class MikezoActivity : AppCompatActivity() {
 
         card1Button.setOnClickListener {
             when (root.currentState) {
+                R.id.fanOut -> {
+                    root.setTransition(R.id.fanOut, R.id.card3OnTop)
+                    root.transitionToEnd()
+                    collapsedCardCompletedListener(R.id.card1OnTop)
+                    midnightPieCheckBox.isVisible = true
+                }
                 R.id.card1OnTop -> {
                     Log.d("Mikezo", "Card 1 already on top")
                 }
@@ -34,6 +43,12 @@ class MikezoActivity : AppCompatActivity() {
         }
         card2Button.setOnClickListener {
             when (root.currentState) {
+                R.id.fanOut -> {
+                    root.setTransition(R.id.fanOut, R.id.card3OnTop)
+                    root.transitionToEnd()
+                    collapsedCardCompletedListener(R.id.card2OnTop)
+                    legumeBlueCheckBox.isVisible = true
+                }
                 R.id.card1OnTop -> {
                     root.setTransition(R.id.card1OnTop, R.id.card2OnTop)
                     root.transitionToEnd()
@@ -53,6 +68,11 @@ class MikezoActivity : AppCompatActivity() {
         }
         card3Button.setOnClickListener {
             when (root.currentState) {
+                R.id.fanOut -> {
+                    root.setTransition(R.id.fanOut, R.id.card3OnTop)
+                    root.transitionToEnd()
+                    hotWaffleCheckBox.isVisible = true
+                }
                 R.id.card1OnTop -> {
                     root.setTransition(R.id.card1OnTop, R.id.card3OnTop)
                     root.transitionToEnd()
@@ -67,9 +87,31 @@ class MikezoActivity : AppCompatActivity() {
                 }
                 R.id.card3OnTop -> {
                     Log.d("Mikezo", "Card 3 already on top")
-
                 }
             }
         }
+    }
+
+    private fun collapsedCardCompletedListener(@IdRes endStateId: Int) {
+        root.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+            override fun allowsTransition(p0: MotionScene.Transition?): Boolean = true
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                if (p1 == R.id.card3OnTop) {
+                    root.setTransition(R.id.card3OnTop, endStateId)
+                    root.transitionToEnd()
+                }
+                root.setTransitionListener(null)
+            }
+        })
     }
 }
