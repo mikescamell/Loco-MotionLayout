b5ef2372a490d449d82dfe29baa7b66fc70772b7
Mike Scamell <mikescamell@gmail.com>
2019-07-23 19:28:25 +0100

    Now you see me, now you don't

--- app/src/main/java/com/mikescamell/motionlayout/loco_motionlayout/Scene1Part3.kt
+++ app/src/main/java/com/mikescamell/motionlayout/loco_motionlayout/Scene1Part3.kt
@@ -3,8 +3,11 @@ package com.mikescamell.motionlayout.loco_motionlayout
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.mikescamell.motionlayout.R
 import com.mikescamell.motionlayout.setLightStatusBar
+import kotlinx.android.synthetic.main.layout1_part3.*
 
 class Scene1Part3 : AppCompatActivity() {
 
@@ -14,6 +17,9 @@ class Scene1Part3 : AppCompatActivity() {
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         setLightStatusBar(window.decorView)
+
+        bookSynopsisCard.isGone = true
+        bookCover.isVisible = true
     }
 
 }
