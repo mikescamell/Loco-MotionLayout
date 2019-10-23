7fe646bfdbbbe9fc57bf766540e3f518af552e08
Mike Scamell <mikescamell@gmail.com>
2019-10-24 00:00:08 +0100

    Don't fade bottom right AVD on lolololol

--- app/src/main/java/com/mikescamell/motionlayout/loco_motionlayout/CombinedSceneLol.kt
+++ app/src/main/java/com/mikescamell/motionlayout/loco_motionlayout/CombinedSceneLol.kt
@@ -1,21 +1,14 @@
 package com.mikescamell.motionlayout.loco_motionlayout
 
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
-import androidx.constraintlayout.motion.widget.TransitionAdapter
-import androidx.core.graphics.ColorUtils
 import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.mikescamell.motionlayout.R
-import com.mikescamell.motionlayout.calculateProgressAlpha
 import com.mikescamell.motionlayout.setLightStatusBar
-import kotlinx.android.synthetic.main.layout2_part4.*
 
 class CombinedSceneLol : AppCompatActivity() {
 
@@ -55,27 +48,6 @@ class CombinedSceneLol : AppCompatActivity() {
             (findViewById<ImageView>(R.id.bookSynopsisFluidBottomRight)).apply {
                 setImageDrawable(bottomRightAnimationForward)
             }
-
-        root.setTransitionListener(object : TransitionAdapter() {
-
-            override fun onTransitionChange(
-                motionLayout: MotionLayout,
-                startId: Int,
-                endId: Int,
-                progress: Float
-            ) {
-                if (startId == R.id.middle) {
-                    val color =
-                        ColorUtils.setAlphaComponent(
-                            Color.WHITE,
-                            calculateProgressAlpha(progress)
-                        )
-                    bottomRightAnimationForward?.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
-                    bottomRightAnimationReverse?.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
-                }
-            }
-
-        })
 
         topLeftAnimationForward?.registerAnimationCallback(object :
             Animatable2Compat.AnimationCallback() {
